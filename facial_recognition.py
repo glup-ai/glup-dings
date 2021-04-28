d374b876faa7dbcdcbf3a360f9ddcda4508690c7
Jon Dahl <jon@dira.no>
2021-04-28 22:35:39 +0200

    Implement video handling and restructure the code
    Implement 'video_to_images' function which reads a video from input
    video filename and saves each frame as an individual jpg. Put the actual
    image processing in a separate function which takes a cv2 image as input
    and saves processed full and crop versions of the image in suitable sub
    directories. Implement 'image_loop' which loops over all images in a
    directory.

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import os
 import numpy as np
 import cv2
@@ -7,37 +8,89 @@ from mtcnn import MTCNN
 # TODO: Implement adjustable face crop.
 
 MIN_CONFIDENCE = 0.97   # Skip all faces under this confidence.
-RAW_DIRECTORY = "fr-imgs-input/"
-PROCESSED_DIRECTORY = "fr-imgs-output/"
+RAW_DIRECTORY = "fr-imgs-input"
+PROCESSED_DIRECTORY = "fr-imgs-output"
 PRINT_INFO = True   # Turn debug info on / off.
+SUPPORTED_IMAGE_FORMATS = ("jpg", "jpeg")
+SUPPORTED_VIDEO_FORMATS = ("mov",)
 
-detector = MTCNN()
-
-for fname_img in sorted(os.listdir(RAW_DIRECTORY)):
+def video_to_images(video_fname):
     """
-    Loop over all images in 'RAW_DIRECTORY'.
+    Save each frame of a video as individual jpg images.  Create a
+    directory with the same name as the video file and save the frames
+    there.
+
+    Warning: Overwrites existing images if run several times for the
+    same video.
+
+    TODO: Input video path instead of only filename.
+    """
+    if (extension := video_fname.split(".")[-1]) not in SUPPORTED_VIDEO_FORMATS:
+        msg = f"Extension '{extension}' is not yet implemented."
+        raise NotImplementedError(msg)
+
+    cap = cv2.VideoCapture(RAW_DIRECTORY + video_fname)
+    
+    if (video_fname.count(".") > 1):
+        msg = f"Video filename cannot contain any periods '.' except for the file extension."
+        raise NotImplementedError(msg)
+    
+    video_fname_no_extension = video_fname.split('.')[0]
+    image_output_directory = f"{RAW_DIRECTORY}{video_fname_no_extension}"
+    os.system(f"mkdir {image_output_directory}")    # TODO: check if the directory already exists (to suppress mkdir message).
+
+    frame_counter = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame_fname = f"{video_fname_no_extension}_{frame_counter:08d}.jpg"
+        
+        if ret:
+            cv2.imwrite(
+                filename = f"{image_output_directory}/{frame_fname}",
+                img = frame
+            )
+        else: break
+        frame_counter += 1
+
+
+def process_image(image, image_fname, output_directory, detector):
+    """
+    Process a single image.
+
+    Parameters
+    ----------
+    image : ?
+        Prob. 3d numpy array.
+
+    image_fname : string
+        Name of the image. Does not need to be the actual filename.
+
+    output_directory : string
+        Path to the directory where the processed images will be saved.
+
+    detector : ?
+        Passed as argument to avoid overhead.
     """
     timing = time.time()
-    image = cv2.cvtColor(cv2.imread(RAW_DIRECTORY + fname_img), cv2.COLOR_BGR2RGB)
     results = detector.detect_faces(image)
     timing = time.time() - timing
 
-    image_total = np.copy(image)
+    image_total = np.copy(image)    # Avoid blue frames when saving cropped faces.
     
-    fname_out = fname_img.split(".")[0] + "_faces"  # Construct output filename.
+    fname_out = image_fname.split(".")[0] + "_faces"  # Construct output filename.
     n_faces = len(results)
     
     if PRINT_INFO:
         print("========================")
-        print(f"{fname_img=}")
+        print(f"{image_fname=}")
         print(f"{image.shape=}")
         print(f"{timing=}")
         print(f"{n_faces=}")
         print(f"image size: {image.size*image.itemsize*1e-6:.1f} MB")
 
     if not results:
-        print(f"No faces detected in image {fname_img}. Skipping.")
-        continue
+        print(f"No faces detected in image {image_fname}. Skipping.")
+        return
 
     for face_idx, result in enumerate(results):
         """
@@ -56,7 +109,7 @@ for fname_img in sorted(os.listdir(RAW_DIRECTORY)):
 
         try:
             cv2.imwrite(    # Save crop of each face.
-                filename = f"{PROCESSED_DIRECTORY}/{fname_out}_crop_{face_idx}.jpg",
+                filename = f"{output_directory}/crop/{fname_out}_crop_{face_idx}.jpg",
                 img = cv2.cvtColor( # Change color space.
                     src = image[
                         bounding_box[1]:bounding_box[1] + bounding_box[3],
@@ -95,7 +148,71 @@ for fname_img in sorted(os.listdir(RAW_DIRECTORY)):
         cv2.circle(image_total, (keypoints['mouth_left']), 2, (0, 155, 255), 2)
         cv2.circle(image_total, (keypoints['mouth_right']), 2, (0, 155, 255), 2)
         
-    cv2.imwrite(f"{PROCESSED_DIRECTORY}_{fname_out}.jpg", cv2.cvtColor(image_total, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(f"{output_directory}/full/{fname_out}.jpg", cv2.cvtColor(image_total, cv2.COLOR_RGB2BGR))
     
     if PRINT_INFO:
         print("========================\n")
+
+
+
+def image_loop(input_path, read_every_n_images=10):
+    """
+    Loop over all images in 'input_path'.
+
+    Parameters
+    ----------
+    input_path : string
+        Path to the directory where the images are stored.
+
+    read_every_n_images : int
+        Read only every n image, skip all others.
+    """
+    if input_path[-1] == "/": input_path = input_path[:-1]  # Remove trailing '/'.
+    
+    detector = MTCNN()
+    input_directory = input_path.split("/")[-1]
+    output_path = f"{PROCESSED_DIRECTORY}/{input_directory}"
+    output_path_full = f"{PROCESSED_DIRECTORY}/{input_directory}/full"
+    output_path_crop = f"{PROCESSED_DIRECTORY}/{input_directory}/crop"
+
+    if not os.path.isdir(output_path):
+        """
+        Create directory for processed images.
+        """
+        os.system(f"mkdir {output_path}")
+    
+    if not os.path.isdir(f"{output_path_full}"):
+        os.system(f"mkdir {output_path_full}")
+
+    if not os.path.isdir(output_path_crop):
+        os.system(f"mkdir {output_path_crop}")
+
+    image_counter = 0
+    for image_fname in sorted(os.listdir(input_path)):
+        """
+        Loop over all images in 'input_path'.
+        """
+        if image_fname.split(".")[-1] not in SUPPORTED_IMAGE_FORMATS:
+            """
+            Read only supported formats.
+            """
+            continue
+        
+        if (image_counter%read_every_n_images != 0):
+            """
+            Read only every n'th image.
+            """
+            image_counter += 1
+            continue
+
+        image = cv2.cvtColor(cv2.imread(f"{input_path}/{image_fname}"), cv2.COLOR_BGR2RGB)
+        process_image(image, image_fname, output_path, detector)
+        image_counter += 1
+
+
+if __name__ == "__main__":
+    input_path = f"{RAW_DIRECTORY}/standalone_images"
+    # input_path = f"{RAW_DIRECTORY}/IMG_6030_720p"
+    image_loop(input_path, read_every_n_images=1)
+    # video_to_images()
+    pass
